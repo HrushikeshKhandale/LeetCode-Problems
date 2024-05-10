64b2a33c49edc33bfe42c5df91673c60ff77eabb
Hrushikesh Khandale <165750849+HrushikeshKhandale@users.noreply.github.com>
2024-05-10 16:04:41 +0530

    Time: 50 ms (75.92%) | Memory: 49.3 MB (45.22%) - LeetSync

---
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function (str1, str2) {
+const gcd = (a, b) => {
+        return b === 0 ? a : gcd(b, a % b);
+    };
+    
+    if (str1 + str2 !== str2 + str1) {
+        return "";
+    }
+    
+    return str1.substring(0, gcd(str1.length, str2.length));
+};
