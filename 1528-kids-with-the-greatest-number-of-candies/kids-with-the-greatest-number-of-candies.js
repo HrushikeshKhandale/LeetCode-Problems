c6f45965ccb65840ff1ac9916c4f6a5110c973bc
Hrushikesh Khandale <165750849+HrushikeshKhandale@users.noreply.github.com>
2024-05-12 23:04:26 +0530

    Time: 53 ms (81.43%) | Memory: 50.2 MB (61.70%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.js
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.js
@@ -4,11 +4,10 @@
  * @return {boolean[]}
  */
 var kidsWithCandies = function(candies, extraCandies) {
-        let maxCandies=Math.max(...candies);
-    
+
         let arr=[];
         for(let i=0;i<candies.length;i++){
-           arr.push(candies[i]+extraCandies>=maxCandies);
+           arr.push(candies[i]+extraCandies>=Math.max(...candies));
         }
 
         
