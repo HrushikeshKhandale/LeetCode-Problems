0f3e508c0d877cf5c54004e1ca5eae4403199c2b
Hrushikesh Khandale <165750849+HrushikeshKhandale@users.noreply.github.com>
2024-05-24 13:03:18 +0530

    Time: 89 ms (46.38%) | Memory: 49.8 MB (59.83%) - LeetSync

---
+++ 1-two-sum/two-sum.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+for (let i = 0; i < nums.length; i++) {
+        for (let j = 1+i; j < nums.length; j++) {
+            if (nums[i] + nums[j] == target) {
+                return [i, j]
+            }
+        }
+    }
+};
