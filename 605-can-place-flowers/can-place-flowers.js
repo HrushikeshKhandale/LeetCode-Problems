783d8efcab8d1e9e0e61311cee20228c54dad6fd
Hrushikesh Khandale <165750849+HrushikeshKhandale@users.noreply.github.com>
2024-06-11 22:42:56 +0530

    Time: 64 ms (28.38%) | Memory: 50.9 MB (68.37%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.js
+++ 605-can-place-flowers/can-place-flowers.js
@@ -5,9 +5,9 @@
  */
 var canPlaceFlowers = function(flowerbed, n) {
   let count = 0;
-  let res;
   for (let i = 0; i < flowerbed.length; i++) {
-if (flowerbed[i] === 0) {
+
+    if (flowerbed[i] === 0) {
       const prevIndex = i === 0 ? 0 : flowerbed[i - 1];
       const nextIndex = i === flowerbed.length - 1 ? 0 : flowerbed[i + 1];
 
